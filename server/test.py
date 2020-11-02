2c687b7e84f182702e4da189cf69e14d4f1968fe
thestarkor <sk_and_mc@kaist.ac.kr>
2020-11-03 00:10:23 +0900

    [fix] url

--- server/test.py
+++ server/test.py
@@ -1,5 +1,5 @@
 import requests
 
-res = requests.post('http://localhost:7000/api', data={'location': 280}).json()
+res = requests.post('http://13.209.217.37/api', data={'location': 280}).json()
 
 print(res['location'])
