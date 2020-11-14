205876ff36afd58ba0c1ad40298fc136b41e5d6c
thestarkor <sk_and_mc@kaist.ac.kr>
2020-11-15 01:33:51 +0900

    [fix] freq

--- server/main.py
+++ server/main.py
@@ -46,7 +46,7 @@ def main():
                         frames = np.concatenate(chunks)
                         direction = mic.get_direction(frames)
                         pixel_ring.set_direction(direction)
-                        res = requests.post('http://13.209.217.37/api', data={'location': int(direction), 'volume': int(rms)}).json()
+                        res = requests.post('http://13.209.217.37/api', data={'location': int(direction), 'volume': int(rms), 'freq': int(val)}).json()
                         print('\ndirection: {} volume: {} frequency: {}'.format(int(direction), int(rms), int(val)))
 
                     speech_count = 0
